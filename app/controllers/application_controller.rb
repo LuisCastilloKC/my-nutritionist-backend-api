1b6412cf802ba244da23767653fe44e437a40d80
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-24 23:55:13 -0400

    add decoded token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::API
         request.headers['Authorizations']
     end
 
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin 
+                JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+        end
+    end
+
 end
