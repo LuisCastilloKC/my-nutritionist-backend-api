22d7decd1e53176eeeb6ebf9d35c06cfe74baa9e
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-24 23:51:03 -0400

    add encode payload token and authorize jwt String

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode(payload, 'my_s3cr3t')
+    end
+
+    def auth_header
+        request.headers['Authorizations']
+    end
+
 end
