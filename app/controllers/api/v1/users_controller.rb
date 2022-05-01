36bb763ee91efad0db86fc62937f78dac6ab2985
Luis Castillo <mr.luiscastillo@gmail.com>
2022-05-01 01:53:02 -0400

    refactor user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,22 @@
 class Api::V1::UsersController < ApplicationController
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
+        user = User.create(user_params)
+        if user.save
+            token = issue_token(user)
+            render json: {user: UserSerializer.new(user), jwt: token}
         else
-            render json: {
-                error: 'failed to create user',
-            },
-            status: :unprocessable_entity
+            if user.errors.messages
+                render json: {error: user.errors.messages}
+            else
+                render json: {error: "User could not be created. Please try again."}
+            end
         end
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:firstname, :lastname, :email, :password)
     end
-
 end
