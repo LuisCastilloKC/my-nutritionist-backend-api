d94d080fbe4950861c6582f70517dfe7da360aeb
Luis Castillo <mr.luiscastillo@gmail.com>
2022-04-30 23:38:25 -0400

    refactor user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, uniqueness: {case_sensitive: false}
     has_many :patients
+    
+    # has_secure_password come from Bcrypt and will validate this data
+    validates :firstname, presence: {message: "First name field is empty."}
+    validates :lastname, presence: {message: "Last name field is empty."}
+    validates :email, uniqueness: {message: "This email is already registered", email: true}
 end
