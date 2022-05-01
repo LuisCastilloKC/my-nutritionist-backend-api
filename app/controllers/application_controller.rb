d9aaf6a9b4b6072ccbf10e2f199acee28c08ffdb
Luis Castillo <mr.luiscastillo@gmail.com>
2022-05-01 01:45:49 -0400

    refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,34 @@
 class ApplicationController < ActionController::API
 
-    before_action :authorized
-
-    def encode_token(payload)
-        JWT.encode(payload, 'my_s3cr3t')
+    def jwt_key
+        Rails.application.credentials.jwt_key
     end
 
-    def auth_header
-        request.headers['Authorizations']
+    def issue_token(user)
+        JWT.encode({user_id: user.id}, jwt_key, "HS256")
     end
 
     def decoded_token
-        if auth_header
-            token = auth_header.split(' ')[1]
-            begin 
-                JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
-            rescue JWT::DecodeError
-                nil
+        begin
+            JWT.decode(token, jwt_key, true, {:algorithm => 'HS256'})
+        rescue => exception
+            [{error: "Invalid Token"}]
         end
     end
 
+    def token
+        request.headers["Authorization"]
+    end
+
+    def user_id
+        decoded_token.first["user_id"]
+    end
+
     def current_user
-        if decoded_token
-            user_id = decoded_token[0]['user_id']
-            @user = User.find_by(id: user_id)
-        end
+        user ||= User.find_by(id: user_id)
     end
 
     def logged_in?
         !!current_user
     end
-
-    def authorized
-        unless logged_in?
-            render json: { message: 'Please Log in' }, status: :unauthorized
-        end
-    end
-
 end
